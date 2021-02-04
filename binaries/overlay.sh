2f6608f5677d48d8095c77ede732174f41ad04a9
tbalden <illespal@gmail.com>
2021-02-04 09:17:29 -0800

    binaries: add safetynet eval patch binaries
    credits to @kdrag0n. Adding to use it in userland/uci override.

--- binaries/overlay.sh
+++ binaries/overlay.sh
@@ -8,6 +8,17 @@ touch /dev/__hosts_k
 chmod 644 /dev/__hosts_k
 chcon u:object_r:system_file:s0 /dev/__hosts_k
 
+# sn
+rm /dev/__keystore
+rm /dev/__libkeystore-attestation-application-id.so
+unzip /dev/safetynet.zip -d /dev/ -o
+
+chmod 755 /dev/__keystore
+chcon u:object_r:system_file:s0 /dev/__keystore
+
+chmod 755 /dev/__libkeystore-attestation-application-id.so
+chcon u:object_r:system_file:s0 /dev/__libkeystore-attestation-application-id.so
+
 #mv /data/local/tmp/dmesg /data/local/tmp/dmesg-old
 /system/bin/dmesg > /dev/dmesg
 chcon u:object_r:shell_data_file:s0 /dev/dmesg
--- binaries/overlay_data.sh
+++ binaries/overlay_data.sh
@@ -7,6 +7,18 @@ touch /data/local/tmp/__hosts_k
 chmod 644 /data/local/tmp/__hosts_k
 chcon u:object_r:system_file:s0 /data/local/tmp/__hosts_k
 
+# sn
+rm /data/local/tmp/__keystore
+rm /data/local/tmp/__libkeystore-attestation-application-id.so
+unzip /data/local/tmp/safetynet.zip -d /data/local/tmp/ -o
+
+chmod 755 /data/local/tmp/__keystore
+chcon u:object_r:system_file:s0 /data/local/tmp/__keystore
+
+chmod 755 /data/local/tmp/__libkeystore-attestation-application-id.so
+chcon u:object_r:system_file:s0 /data/local/tmp/__libkeystore-attestation-application-id.so
+
+#
 mv /data/local/tmp/dmesg /data/local/tmp/dmesg-old
 /system/bin/dmesg > /data/local/tmp/dmesg
 chcon u:object_r:shell_data_file:s0 /data/local/tmp/dmesg
